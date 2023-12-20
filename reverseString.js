9cd199a2ce75b822d630e286609d40c4c8779aab
Hassan Hafez <laptop@laptops-MacBook.local>
2023-12-20 14:35:07 +0200

    rev string solution 1

---
+++ reverseString.js
@@ -0,0 +1,26 @@
+/*
+Reverse string
+example: 
+apple => elppa
+greetings => sgniteerg
+*/
+
+/* Solution 1
+1 - given string - convert into array
+2 - reverse the array
+3 - turn the array back into string
+*/
+
+const revString = (str) => {
+	return str.split("").reverse().join("");
+};
+
+const result = revString("Greetings!");
+
+console.log("result", result);
+
+/* Solution 2
+1 - given string - create empty string
+2 - loop through each character
+3 - return the reversed
+*/
