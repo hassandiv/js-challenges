71debfb461e4c0d0b00625611d50a346f095e529
Hassan Hafez <laptop@laptops-MacBook.local>
2023-12-20 20:09:20 +0200

    palindrome

---
+++ palindrome.js
@@ -0,0 +1,17 @@
+const Palindrome = (str) => {
+	const isPalindrome = str.split("").reverse().join("");
+	return str === isPalindrome;
+};
+const palindrome = Palindrome("HannaH");
+console.log("palindrome", palindrome);
+
+(function (str) {
+	let reversed = "";
+	console.log("let reversed", reversed);
+	for (let x of str) {
+		console.log("x", x);
+		reversed = x + reversed;
+		console.log("reversed", reversed);
+	}
+	console.log("reversed", reversed === str);
+})("AbbA");
