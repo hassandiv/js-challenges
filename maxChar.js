ef7cc3ef8899ee613ee406a631df5dc7d35d6cda
Hassan Hafez <laptop@laptops-MacBook.local>
2023-12-20 22:08:11 +0200

    created an object, added the props + values, then find the maxNum + maxChar

---
+++ maxChar.js
@@ -0,0 +1,26 @@
+/* 
+  32eddseew3
+  #%he3#33ll#l#o
+*/
+//find the character that appears the most and total number of times it appears
+
+const maxChar = (str) => {
+	let obj = {};
+
+	for (let x of str) {
+		!obj[x] ? (obj[x] = 1) : obj[x]++;
+	}
+	console.log("obj", obj);
+
+	let maxNum = 0;
+	let maxChar = "";
+
+	for (let char in obj) {
+		if (obj[char] >= maxNum) {
+			maxNum = obj[char];
+			maxChar = char;
+		}
+	}
+	console.log(`${maxChar} appears ${maxNum} times`);
+};
+maxChar("#%he3#33l3l#l#3xox");
